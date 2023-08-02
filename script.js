c1149ec9358d6fde087dfd945e7ed31d09cae576
Suryansh Parashar <127846351+thesuryanshparashar@users.noreply.github.com>
2023-08-02 22:24:02 +0530

    Initial upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let displayValue = "0";
+
+function updateDisplay() {
+  const display = document.getElementById("display");
+  display.textContent = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "0";
+  updateDisplay();
+}
+
+function appendToDisplay(value) {
+  if (displayValue === "0") {
+    displayValue = value;
+  } else {
+    displayValue += value;
+  }
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue).toString();
+  } catch (error) {
+    displayValue = "Error";
+  }
+  updateDisplay();
+}
+
+updateDisplay();
